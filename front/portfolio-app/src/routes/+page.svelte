4b97e5f64e57fdfbcc1d0ce5ed023f219112c478
Samuel Petre <petresamuel@gmail.com>
2023-09-07 22:56:38 +0200

    work in progress - building frontend website

--- front/portfolio-app/src/routes/+page.svelte
+++ front/portfolio-app/src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import urlSlug from "url-slug";
+    import { format } from "timeago.js";
+    import Project from "./projects/+page.svelte";
+    import { blogs } from "../data.js";
+</script>
+
+<h1>All your traveling tips in one place</h1>
+{#each blogs as blog, i}
+    <Project title={blog.title} description={blog.content} image={blog.image} publishDate={format(blog.publishDate)}/>
+{/each}
