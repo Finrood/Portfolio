b79cbc309f49519953686c331f64cab3381f0a7a
Samuel Petre <petresamuel@gmail.com>
2024-05-08 19:29:07 -0300

    [Back] - Optimize imports

--- server/src/main/java/com/example/portfolio/repository/ProfileRepository.java
+++ server/src/main/java/com/example/portfolio/repository/ProfileRepository.java
@@ -1,11 +1,9 @@
 package com.example.portfolio.repository;
 
 import com.example.portfolio.model.Profile;
-import com.example.portfolio.model.Project;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
